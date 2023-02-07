c2ef782db329f8d7d22691fa44b592eb706f5e81
Juan Ignacio Mascarenhas <108559438+JIM90mdp@users.noreply.github.com>
2023-02-07 08:06:55 -0300

    defaultValue - validations (#16)

--- src/admin/location/location-create.jsx
+++ src/admin/location/location-create.jsx
@@ -1,9 +1,17 @@
 import { Create, SimpleForm, TextInput, SelectInput, required } from 'react-admin';
 import { Box } from '@mui/material';
 
+const validateUserCreation = (values) => {
+    const errors = {};
+    if (!values.name) {
+        errors.name = 'The Name is required';
+    }
+     return errors
+  };
+
 export const LocationCreate = () => (
     <Create>
-        <SimpleForm sx={{ maxWidth: 500 }}>
+        <SimpleForm sx={{ maxWidth: 500 }} validate={validateUserCreation}>
             <Box display={{ xs: 'block', sm: 'flex', width: '100%' }}>
                 <Box flex={2} mr={{ xs: 0, sm: '0.5em' }}>
                     <TextInput source="name"
@@ -15,6 +23,7 @@ export const LocationCreate = () => (
                     <SelectInput source="status"
                         label="Status"
                         optionValue="name"
+                        defaultValue={"Active"}
                         // emptyText="Select an option"
                         // emptyValue="Select an option"
                         choices={[
