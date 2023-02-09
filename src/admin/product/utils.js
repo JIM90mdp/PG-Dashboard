1ea524eff7812915188b438fd224287fe91c87b4
soyfriss <federico.rissone@gmail.com>
2023-02-09 13:37:44 -0300

    refactor(dash/product): validate product
    Add validations before creating/updating a product

--- src/admin/product/utils.js
+++ src/admin/product/utils.js
@@ -1,12 +1,26 @@
 export function parseTime(value) {
-    console.log('parseTime, value: ', value);
+    // console.log('parseTime, value: ', value);
     if (!value) return null;
 
     return value;
 }
 
 export function formatTime(value) {
-    console.log('formatTime, value: ', value);
+    // console.log('formatTime, value: ', value);
+    if (value == null) return '';
+
+    return value;
+}
+
+export function parseDate(value) {
+    console.log('parseDate, value: ', value);
+    if (!value) return null;
+
+    return value;
+}
+
+export function formatDate(value) {
+    console.log('formatDate, value: ', value);
     if (value == null) return '';
 
     return value;
