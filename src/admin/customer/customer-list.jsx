44141b1c60e8b374a9c021ba09e4a3c8a9b49235
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-02-10 13:23:50 -0300

    refacotr/customer-list-responsive

--- src/admin/customer/customer-list.jsx
+++ src/admin/customer/customer-list.jsx
@@ -1,40 +1,58 @@
 import {
-    Datagrid,
-    List,
-    TextField,
-    TextInput,
-    SelectInput,
-    ReferenceField,
-    DateField,
-    NumberField,
-    EmailField,
-    FunctionField
-} from 'react-admin';
+  Datagrid,
+  List,
+  TextField,
+  TextInput,
+  SelectInput,
+  ReferenceField,
+  DateField,
+  NumberField,
+  EmailField,
+  FunctionField,
+  SimpleList,
+} from "react-admin";
+import { useMediaQuery } from "@mui/material";
 
-const customerFilters = [
+export const CustomerList = () => {
+  const isSmall = useMediaQuery((theme) => theme.breakpoints.down("sm"));
+
+  const customerFilters = [
     <TextInput source="name" label="Search" alwaysOn />,
-    <SelectInput source="status" label="Status" choices={[
+    <SelectInput
+      source="status"
+      label="Status"
+      choices={[
         { id: "Active", name: "Active" },
-        { id: "Disabled", name: "Disabled" }
-    ]} alwaysOn />
-];
-
-export const CustomerList = () => (
+        { id: "Disabled", name: "Disabled" },
+      ]}
+      alwaysOn
+    />,
+  ];
+  return (
     <List filters={customerFilters}>
+      {isSmall ? (
+        <SimpleList
+          primaryText={(customer) => customer.name}
+          secondaryText={(customer) => customer.telephone}
+          tertiaryText={(customer) => customer.email}
+        />
+      ) : (
         <Datagrid rowClick="edit" bulkActionButtons={false}>
-            <TextField source="name" />
-            <ReferenceField source="userId" reference="users">
-                <TextField source="id" />
-            </ ReferenceField>
-            <TextField source="address" />
-            <TextField source="city" />
-            <TextField source="state" />
-            <NumberField  source="zip" />
-            <EmailField source="email" />
-            <NumberField  source="telephone" />
-            <NumberField  source="document" />
-            <DateField  source="birthDate" />
-            <TextField source="status" />
+          <TextField source="name" />
+          <ReferenceField source="userId" reference="users">
+            <TextField source="id" />
+          </ReferenceField>
+          <TextField source="address" lebel="Direcciòn" />
+          <TextField source="city" lebel="Ciudad" />
+          <TextField source="state" lebel="Provincia" />
+          <NumberField source="zip" lebel="Código Postal" />
+          <EmailField source="email" lebel="Email" />
+          <NumberField source="telephone" lebel="Teléfono" />
+          <NumberField source="document" lebel="Documento" />
+          <DateField source="birthDate" lebel="Fecha de nacimiento" />
+          <TextField source="status" lebel="Estado" />
         </Datagrid>
+      )}
     </List>
-);
+  );
+};
