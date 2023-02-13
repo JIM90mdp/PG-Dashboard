86cec7f32e4d0f6cca0eda1f7a1a3a9594e548eb
soyfriss <federico.rissone@gmail.com>
2023-02-13 08:44:40 -0300

    refactor(dash/user): default values on user creation

--- src/admin/user/user-create.jsx
+++ src/admin/user/user-create.jsx
@@ -9,10 +9,14 @@ import {
 } from "react-admin";
 import { Box } from "@mui/material";
 
+const userDefaultValues = () => ({
+  role: 'User',
+  status: 'Active'
+});
 
 export const UserCreate = () => (
   <Create>
-    <SimpleForm sx={{ maxWidth: 500 }}>
+    <SimpleForm defaultValues={userDefaultValues} sx={{ maxWidth: 500 }}>
       <Box display={{ xs: "block", sm: "flex", width: "100%" }}>
         <Box flex={2} mr={{ xs: 0, sm: "0.5em" }}>
           <TextInput source="userName" label="Nombre" validate={required()} fullWidth />
@@ -22,7 +26,6 @@ export const UserCreate = () => (
             source="role"
             label="Rol"
             optionValue="name"
-            // defaultValue={"User"}
             choices={[
               { role: "User", name: "User" },
               { role: "Admin", name: "Admin" },
@@ -36,7 +39,6 @@ export const UserCreate = () => (
             source="status"
             label="Estado"
             optionValue="name"
-            // defaultValue={"Active"}
             choices={[
               { id: "Active", name: "Active" },
               { id: "Disabled", name: "Disabled" },
