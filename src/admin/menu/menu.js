bf9ee6f7e40732874c66ae73503cf09341466d31
JIM90mdp <juanignaciomascarenhas@gmail.com>
2023-02-13 21:12:54 -0300

    refactor/dash-menu-plural

--- src/admin/menu/menu.js
+++ src/admin/menu/menu.js
@@ -1,9 +1,7 @@
 import * as React from "react";
 import { DashboardMenuItem, MenuItemLink } from "react-admin";
 import BookIcon from "@mui/icons-material/Book";
-import ChatBubbleIcon from "@mui/icons-material/ChatBubble";
 import PeopleIcon from "@mui/icons-material/People";
-import LabelIcon from "@mui/icons-material/Label";
 import LocationOnIcon from '@mui/icons-material/LocationOn';
 import GroupsIcon from '@mui/icons-material/Groups';
 import CategoryIcon from '@mui/icons-material/Category';
@@ -15,19 +13,19 @@ export const Menu = () => (
   <div>
     <DashboardMenuItem />
     <MenuItemLink to="/artists" primaryText="Artists" leftIcon={<GroupsIcon />} />
-    <MenuItemLink to="/locations" primaryText="Location" leftIcon={<LocationOnIcon />} />
-    <MenuItemLink to="/categories" primaryText="Category" leftIcon={<CategoryIcon />} />
-    <MenuItemLink to="/products" primaryText="Product" leftIcon={<SlideshowIcon />} />
+    <MenuItemLink to="/locations" primaryText="Locations" leftIcon={<LocationOnIcon />} />
+    <MenuItemLink to="/categories" primaryText="Categories" leftIcon={<CategoryIcon />} />
+    <MenuItemLink to="/products" primaryText="Products" leftIcon={<SlideshowIcon />} />
     <MenuItemLink
       to="/users"
       primaryText="User"
       leftIcon={<PeopleIcon />}
     />
-    <MenuItemLink to="/customers" primaryText="Customer" leftIcon={<WifiCalling3Icon />} />
-    <MenuItemLink to="/orders" primaryText="Order" leftIcon={<BookmarkBorderIcon />} />
+    <MenuItemLink to="/customers" primaryText="Customers" leftIcon={<WifiCalling3Icon />} />
+    <MenuItemLink to="/orders" primaryText="Orders" leftIcon={<BookmarkBorderIcon />} />
     <MenuItemLink
       to="/reviews"
-      primaryText="Review"
+      primaryText="Reviews"
       leftIcon={<BookIcon />}
     />
   </div>
